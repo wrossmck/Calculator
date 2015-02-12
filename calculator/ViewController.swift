60b912d21abc1ddf46f2a25828fab66d1043d035
Ross McKinley <wrossmck@gmail.com>
2015-02-12 22:04:01 +0000

    Lecture 1

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -2,13 +2,26 @@
 //  ViewController.swift
 //  calculator
 //
-//  Created by Ross McKinley on 12/02/2015.
+//  Created by Ross McKinley on 10/02/2015.
 //  Copyright (c) 2015 Ross McKinley. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+	
+	@IBOutlet weak var display: UILabel!
+	
+	var userIsInTheMiddleOfTypingNumber: Bool = false
+	
+	@IBAction func appendDigit(sender: UIButton) {
+		let digit = sender.currentTitle!
+		if userIsInTheMiddleOfTypingNumber {
+			display.text = display.text! + digit
+		} else {
+			display.text = digit
+			userIsInTheMiddleOfTypingNumber = true
+		}
+	}
 }
 
