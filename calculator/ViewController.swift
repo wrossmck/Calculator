4072d8728b9101a3a2edd9a80a39fda311ff036b
Ross McKinley <wrossmck@gmail.com>
2015-02-13 00:02:55 +0000

    Lecture 2

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 	
 	@IBOutlet weak var display: UILabel!
 	
-	var userIsInTheMiddleOfTypingNumber: Bool = false
+	var userIsInTheMiddleOfTypingNumber = false
 	
 	@IBAction func appendDigit(sender: UIButton) {
 		let digit = sender.currentTitle!
@@ -21,6 +21,58 @@ class ViewController: UIViewController {
 		} else {
 			display.text = digit
 			userIsInTheMiddleOfTypingNumber = true
+		}
+	}
+	
+	
+	
+	@IBAction func operate(sender: UIButton) {
+		let operation = sender.currentTitle!
+		if userIsInTheMiddleOfTypingNumber{
+			enter()
+		}
+		switch operation {
+		case "×": perform { $1*$0 }
+		case "÷": perform { $1/$0 }
+		case "+": perform { $1+$0 }
+		case "-": perform { $1-$0 }
+		case "√": perform { sqrt($0) }
+			default: break
+		}
+	}
+	
+	func perform(operation: (Double, Double) -> Double){
+		if operandStack.count >= 2 {
+			displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+			enter()
+		}
+	}
+	func perform(operation: (Double) -> Double){
+		if operandStack.count >= 1 {
+			displayValue = operation(operandStack.removeLast())
+			enter()
+		}
+	}
+	
+	var operandStack = Array<Double>()
+	@IBAction func enter() {
+		userIsInTheMiddleOfTypingNumber = false
+		operandStack.append(displayValue)
+		println("\(operandStack)")
+	}
+	
+	
+	
+	
+	
+	
+	var displayValue: Double {
+		get {
+			return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+		}
+		set {
+			display.text = "\(newValue)"
+			userIsInTheMiddleOfTypingNumber = false
 		}
 	}
 }
